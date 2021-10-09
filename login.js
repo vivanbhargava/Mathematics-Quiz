90f7f7844d71dd66720e188057e03c1ee6280857
Vivan Bhargava <78213755+vivanbhargava@users.noreply.github.com>
2021-10-09 10:16:56 +0530

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    player_name1 = document.getElementById("player_name1").value;
-    player_name2 = document.getElementById("player_name2").value;
-    localStorage.setItem("Player 1 Name: ", player_name1);
-    localStorage.setItem("Player 2 Name:", player_name2);
-    window.location="game.html";
-}
